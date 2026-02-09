db3833d56e41848b8ac9fbd45a05775324476d06
이름 <>
2026-02-09 22:16:27 +0900

    초기 커밋

---
+++ src/main/java/spartaclub/planit2/PlanIt2Application.java
@@ -0,0 +1,13 @@
+package spartaclub.planit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanIt2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanIt2Application.class, args);
+    }
+
+}
---
+++ src/test/java/spartaclub/planit2/PlanIt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package spartaclub.planit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanIt2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
